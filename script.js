29ca74709a9201cd0902d57fa97c368cf255f571
Filipuci <rafaellifipuci@outlook.com>
2024-09-14 17:47:36 -0300

    add players in table

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
-const randomizerArea = document.querySelector('#randomizer-area')
-const randomizedPlayers = []
-const attackerPlayers = []
-const defendersPlayers = []
+let randomizedPlayers = []
 
-
-
-
-
-
-
+function numeroAleatorio(max) {
+  return Math.floor(Math.random() * max)
+}
 
 document.querySelector('#randomizer-button').addEventListener('click', () => {
+  let attackerPlayers = []
+  let defendersPlayers = []
+  randomizedPlayers = []
+  
   // Pegar o input.value(os nomes escritos no textarea)
-  const players = randomizerArea.value.split('\n')
+  const players = document.querySelector('#randomizer-area').value.split('\n')
+  const max = players.length
 
-  function numeroAleatorio() {
-    return Math.floor(Math.random() * players.length)
-  }
-  
-  function shuffle(array) {
-    for (let i = 0; i < players.length; i++) {
-      let j = numeroAleatorio()
-      while (randomizedPlayers.indexOf(players[j]) != -1) {
-        j = numeroAleatorio()
-      }
-  
-      randomizedPlayers.push(players[j])
+  // Sortear os nomes dos X nomes escritos
+  for (let i = 0; i < players.length; i++) {
+    let j = numeroAleatorio(max)
+    while (randomizedPlayers.indexOf(players[j]) != -1) {
+      j = numeroAleatorio(max)
     }
+
+    randomizedPlayers.push(players[j])
   }
-  
-  shuffle()
-  
+
+  // Definir quem vai estar dentro de attackersPlayers e quem vai estar dentro de defendersPlayers
   randomizedPlayers.forEach((player, i) => {
     if (i < 5) {
       attackerPlayers.push(player)
@@ -38,13 +31,23 @@ document.querySelector('#randomizer-button').addEventListener('click', () => {
       defendersPlayers.push(player)
     }
   })
-
-  console.log(attackerPlayers)
-  console.log(defendersPlayers)
-
-
-  // Sortear os nomes dos X nomes escritos
-
-
-  // Adicionar os nomes sorteador na tabela de attackers and defenders
+  updatePlayerNames(attackerPlayers, defendersPlayers)
 })
+
+
+
+// Adicionar os nomes sorteador na tabela de attackers and defenders
+function updatePlayerNames(attackerPlayers, defendersPlayers) {
+  const playerAttack = document.querySelectorAll('.player-attack')
+  const playerDefender = document.querySelectorAll('.player-defense')
+
+  playerAttack.forEach(player => {
+    const playerIndex = player.dataset.player
+    player.innerHTML = attackerPlayers[playerIndex]
+  })
+
+  playerDefender.forEach(player => {
+    const playerIndex = player.dataset.player
+    player.innerHTML = defendersPlayers[playerIndex]
+  })
+}
