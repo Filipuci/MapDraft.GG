4d64665c569ba84b5f8009061f0724b20f5f3b06
Filipuci <rafaellifipuci@outlook.com>
2024-10-01 08:08:26 -0300

    v2.0.0

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.querySelector('#randomizer-button').addEventListener('click', () => {
 
   // Pegar o input.value (os nomes escritos no textarea)
   const players = document.querySelector('#randomizer-area').value.split('\n');
-  
+
   // Embaralhar os jogadores
   randomizedPlayers = shuffle(players);
 
@@ -28,20 +28,206 @@ document.querySelector('#randomizer-button').addEventListener('click', () => {
   });
 
   updatePlayerNames(attackerPlayers, defendersPlayers);
+
+  if (attackerPlayers.length !== 0 && defendersPlayers.length !== 0) {
+    document.querySelector('#confirm-btn').addEventListener('click', () => {
+      document.querySelector('#randomizer-container').style.display = 'none'
+      document.querySelector('#picksAndBans-container').style.display = 'flex'
+      document.querySelector('#confirm-btn').style.display = 'none'
+    })
+  }
 });
+
+
+function updateBodyBackgroundPick(map) {
+  const mapName = map.getAttribute('data-map')
+  document.body.style.background = `url(img/${mapName}.webp)`
+}
+
+function updateBodyBackgroundBan(map) {
+  const mapName = map.getAttribute('data-map')
+  document.body.style.background = `linear-gradient(rgba(17, 17, 17, 0.8), rgba(20, 20, 20, 1)), url(img/${mapName}.webp)`
+}
+
+const banPickPhase = document.querySelector('#banPick-phase')
+
+let isSubmitted = false
+
+function updateBanPickPhaseMd3(clickCount) {
+
+  if (isSubmitted) {
+    banPickPhase.innerHTML = 'Mapas escolhidos'
+    return
+  }
+
+  if (clickCount < 2) {
+    banPickPhase.innerHTML = 'Fase de banimento'
+  }
+  else if (clickCount < 4) {
+    banPickPhase.innerHTML = 'Fase de escolha'
+  }
+  else if (clickCount < 6) {
+    banPickPhase.innerHTML = 'Fase de banimento'
+  }
+  else if (clickCount === 6) {
+    banPickPhase.innerHTML = 'Escolha o último mapa disponivel'
+  }
+  else if (clickCount === 7) {
+    banPickPhase.innerHTML = 'Último mapa'
+  }
+}
+
+function updateBanPickPhaseMd5(clickCount) {
+
+  if (isSubmitted) {
+    banPickPhase.innerHTML = 'Mapas escolhidos'
+    return
+  }
+
+  if (clickCount < 2) {
+    banPickPhase.innerHTML = 'Fase de banimento'
+  }
+  else if (clickCount < 6) {
+    banPickPhase.innerHTML = 'Fase de escolha'
+  }
+  else if (clickCount === 6) {
+    banPickPhase.innerHTML = 'Escolha o último mapa disponivel'
+  }
+  else if (clickCount === 7) {
+    banPickPhase.innerHTML = 'Último mapa'
+  }
+}
+
+function handleMapClickMd3(map, clickCount) {
+  const img = map.querySelector('img')
+  img.style.filter = 'brightness(0.3)'
+
+  if (clickCount <= 2) {
+    map.classList.add('ban')
+    updateBodyBackgroundBan(map)
+  }
+  else if (clickCount <= 4) {
+    map.classList.add('pick')
+    updateBodyBackgroundPick(map)
+  }
+  else if (clickCount <= 6) {
+    map.classList.add('ban')
+    updateBodyBackgroundBan(map)
+  }
+  else if (clickCount <= 7) {
+    map.classList.add('pick')
+    updateBodyBackgroundPick(map)
+  }
+}
+
+function handleMapClickMd5(map, clickCount) {
+  const img = map.querySelector('img')
+  img.style.filter = 'brightness(0.3)'
+
+  if (clickCount <= 2) {
+    map.classList.add('ban')
+    updateBodyBackgroundBan(map)
+  }
+  else if (clickCount <= 7) {
+    map.classList.add('pick')
+    updateBodyBackgroundPick(map)
+  }
+}
+
+function deselectMap(map) {
+  const img = map.querySelector('img')
+
+  if (map.classList.contains('ban')) {
+    map.classList.remove('ban');
+  } else if (map.classList.contains('pick')) {
+    map.classList.remove('pick');
+  }
+
+  img.style.filter = 'none';
+}
+
+const maps = document.querySelectorAll('[data-map]')
+let clickCount = 0
+
+document.querySelector('#md3').addEventListener('click', () => {
+  updateBanPickPhaseMd3(clickCount)
+  maps.forEach(map => {
+    map.addEventListener('click', () => {
+      if (map.classList.contains('ban') || map.classList.contains('pick')) {
+        clickCount--
+        deselectMap(map)
+      } else {
+        clickCount++
+        handleMapClickMd3(map, clickCount);
+      }
+
+      updateBanPickPhaseMd3(clickCount)
+
+      if (clickCount === 7) {
+        document.querySelector('.submit-btn').addEventListener('click', () => {
+          isSubmitted = true
+          banPickPhase.innerHTML = 'Mapas escolhidos'
+
+          document.querySelectorAll('.ban').forEach(bannedMap => {
+            bannedMap.style.display = 'none'
+            document.querySelectorAll('.pick img').forEach(pickedMap => {
+              pickedMap.style.width = '220px'
+              pickedMap.style.height = '160px'
+            })
+          })
+        })
+      }
+    }
+    )
+  })
+})
+
+document.querySelector('#md5').addEventListener('click', () => {
+  updateBanPickPhaseMd5(clickCount)
+  maps.forEach(map => {
+    map.addEventListener('click', () => {
+
+      if (map.classList.contains('ban') || map.classList.contains('pick')) {
+        clickCount--
+        deselectMap(map)
+
+      } else {
+        clickCount++
+        handleMapClickMd5(map, clickCount);
+      }
+
+      updateBanPickPhaseMd5(clickCount)
+
+      if (clickCount === 7) {
+        document.querySelector('.submit-btn').addEventListener('click', () => {
+          isSubmitted = true
+          banPickPhase.innerHTML = 'Mapas escolhidos'
+          document.querySelectorAll('.ban').forEach(bannedMap => {
+            bannedMap.style.display = 'none'
+            document.querySelectorAll('.pick img').forEach(pickedMap => {
+              pickedMap.style.width = '220px'
+              pickedMap.style.height = '160px'
+            })
+          })
+        })
+      }
+    }
+    )
+  })
+})
 
 // Função para atualizar os nomes na tabela de attackers e defenders
 function updatePlayerNames(attackerPlayers, defendersPlayers) {
-  const playerAttack = document.querySelectorAll('.player-attack');
-  const playerDefender = document.querySelectorAll('.player-defense');
+  const playerAttack = document.querySelectorAll('.player-attack')
+  const playerDefender = document.querySelectorAll('.player-defense')
 
   playerAttack.forEach(player => {
     const playerIndex = player.dataset.player;
-    player.innerHTML = attackerPlayers[playerIndex] || ''; // Para lidar com menos de 5 jogadores
+    player.innerHTML = attackerPlayers[playerIndex] || ''
   });
 
   playerDefender.forEach(player => {
     const playerIndex = player.dataset.player;
-    player.innerHTML = defendersPlayers[playerIndex] || ''; // Para lidar com menos de 5 jogadores
+    player.innerHTML = defendersPlayers[playerIndex] || ''
   });
 }
